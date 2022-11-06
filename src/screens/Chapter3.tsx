edd72552eb33920a3d70e43602d532082a388191
Joseph Kim <cloudeyes@gmail.com>
2022-11-06 22:52:23 +0900

    Add icons

--- src/screens/Chapter3.tsx
+++ src/screens/Chapter3.tsx
@@ -6,8 +6,11 @@ import {
   SafeAreaView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import * as D from '../data';
 
@@ -17,22 +20,41 @@ const avatarUrl = D.randomAvatarUrl();
 const avatarSize = 36;
 
 const {width, height} = Dimensions.get('window');
+const handleIconPress = () => {
+  console.log('icon pressed');
+};
+
 const Chapter3 = () => (
   <SafeAreaView style={[styles.safeAreaView]}>
-    <Text style={[styles.text]}>os: {Platform.OS}</Text>
-    <Text style={[styles.text]}>width: {width}px</Text>
+    <Text style={[styles.text, fontStyles.regular]}>os: {Platform.OS}</Text>
+    <Text style={[styles.text, fontStyles.bold]}>width: {width}px</Text>
     <Text style={[styles.text]}>height: {height}px</Text>
-    <Text style={[styles.customText]}>Hello, World!</Text>
+    <Text style={[styles.customText, fontStyles.regular]}>Hello, World!</Text>
+    <Text style={[styles.customTextBold, fontStyles.bold]}>Hello, World!</Text>
     <View style={[styles.box, styles.border]} />
     <View style={[styles.box, styles.border, {borderRadius: 10}]} />
     <Image source={{uri: avatarUrl}} style={[styles.box, styles.image]} />
+    <TouchableOpacity style={[styles.base]} onPress={handleIconPress}>
+      <Icon name="home" size={50} color={Colors.lightBlue500} />
+    </TouchableOpacity>
   </SafeAreaView>
 );
 
 const paddingLeft = 20;
+const fontStyles = StyleSheet.create({
+  regular: {
+    fontWeight: '400',
+  },
+  bold: {
+    fontWeight: Platform.select({ios: '700', android: '600'}),
+  },
+});
 const styles = StyleSheet.create({
+  base: {
+    marginLeft: Platform.select({ios: paddingLeft}),
+  },
   safeAreaView: {
-    backgroundColor: Colors.blue500,
+    backgroundColor: Colors.blue100,
     flex: 1,
     paddingLeft: Platform.select({android: paddingLeft}),
     paddingRight: Platform.select({android: paddingLeft}),
@@ -42,6 +64,11 @@ const styles = StyleSheet.create({
     marginLeft: Platform.select({ios: paddingLeft}),
   },
   customText: {
+    fontSize: 24,
+    marginLeft: Platform.select({ios: paddingLeft}),
+    fontFamily: 'DancingScript-Regular',
+  },
+  customTextBold: {
     fontSize: 24,
     marginLeft: Platform.select({ios: paddingLeft}),
     fontFamily: 'DancingScript-Bold',
@@ -61,6 +88,9 @@ const styles = StyleSheet.create({
     width: avatarSize,
     height: avatarSize,
     borderRadius: avatarSize / 2,
+  },
+  icon: {
+    fontFamily: 'Material Community Icons',
   },
 });
 
